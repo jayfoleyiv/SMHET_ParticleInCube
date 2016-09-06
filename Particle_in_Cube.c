22a7c24b6114a660c5e73b98d0b35875814f97a1
eldabaghn <eldabaghn@student.wpunj.edu>
2016-09-06 16:54:16 -0400

    Update Particle_in_Cube.c
    Changed nmax to 7

--- Particle_in_Cube.c
+++ Particle_in_Cube.c
@@ -94,7 +94,7 @@ int main() {
         double Ei, Eg;
 
         // nx ny nz equal to three (3)
-        nmax = 8;
+        nmax = 7;
         // number of electrons
         Nels = 472;
         // number of occupied orbitals
